a53f3adfdfd06aacf972bee18839c51dc67a0157
Salman <khansalman752@gmail.com>
2017-02-09 16:09:19 +0530

    programs

---
+++ Practice/src/powerof2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class powerof2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// read in one command-line argument
+        int n = Integer.parseInt(args[0]);
+
+        int i = 0;         	
+        System.out.println("Enter ");
+        int powerOfTwo = 1;       
+
+        if(n>=0 && n<31)
+        {
+        while (i <= n) {
+            System.out.println(i + " " + powerOfTwo);  
+            powerOfTwo = 2 * powerOfTwo;          
+            i = i + 1;
+        }
+        }
+        else
+        	System.out.println("Input must be between 0 t0 31");
+	
+	}
+}
