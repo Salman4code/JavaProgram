b55730e1547fc36192d74bc96852ca1630f997d9
Salman <khansalman752@gmail.com>
2017-02-09 17:54:08 +0530

    Update

--- Practice/src/prime.java
+++ Practice/src/prime.java
@@ -3,10 +3,30 @@ public class prime {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n=10;
-		for (int i = 0; i < n; i++) {
-			
-		}
-	}
+		int n = 63, sum = 0;
+		int flag;
+		for (int i = 1; i < n; i++)
+		{
+			flag = 0;
+			for (int j = 2; j < i/2; j++) 
+			{
 
+				if (i % j == 0) 
+				{
+					flag ++;
+					break;
+				}
+			}
+				if (flag == 0) {
+					
+				if(n%i==0){
+					System.out.println(i);
+				}
+				//	sum = sum + i;
+				}
+
+			}
+		
+//System.out.println(sum);
+	}
 }
