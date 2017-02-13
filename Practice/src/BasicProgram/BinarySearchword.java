787591be9daf148e6e19771ac1a43df4c789bdc9
Salman <khansalman752@gmail.com>
2017-02-13 17:21:39 +0530

    Changes

--- Practice/src/BasicProgram/BinarySearchword.java
+++ Practice/src/BasicProgram/BinarySearchword.java
@@ -16,29 +16,30 @@ public class BinarySearchword {
 		// TODO Auto-generated method stub
 		String arr1[];
 		Scanner sc = new Scanner(System.in);
-		//System.out.println("Enter How many Alphabate in word you want to enter");
-	//	int n = sc.nextInt();
-		//arr1 = new String[n];
+		// System.out.println("Enter How many Alphabate in word you want to
+		// enter");
+		// int n = sc.nextInt();
+		// arr1 = new String[n];
 
 		InputStream in = new FileInputStream(new File("/home/bridgeit/workspace/Practice/src/BasicProgram/abc.txt"));
 
 		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-String strp= "";
+		String strp = "";
 		StringBuilder out = new StringBuilder();
-		String line;
+		String line = "";
 		while ((line = reader.readLine()) != null) {
-			out.append(line);
-			strp+=line;
-			
+			// out.append(line);
+			strp += line;
+
 		}
-		System.out.println(out.toString()); // Prints the string content read						// from input stream
+		System.out.println(out.toString()); // Prints the string content read //
+											// from input stream
 		reader.close();
 		String str[] = strp.split(" ");
-	/*	System.out.println("Enter String alphabtes");
-		for (int i = 0; i <str.length; i++) {
-			str[i]=sc.next();
-			//arr1[i] = sc.next();
-		}*/
+		/*
+		 * System.out.println("Enter String alphabtes"); for (int i = 0; i
+		 * <str.length; i++) { str[i]=sc.next(); //arr1[i] = sc.next(); }
+		 */
 		Arrays.sort(str);
 		System.out.println("Sorted Order for String is");
 		for (int i = 0; i < str.length; i++) {
